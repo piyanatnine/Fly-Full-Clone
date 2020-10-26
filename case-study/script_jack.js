abb95aa605a4dbcda197fedf7801eb4daa790f49
unknown <62070088@kmitl.ac.th>
2020-10-26 19:01:33 +0700

    Removed debugging js

--- case-study/script_jack.js
+++ case-study/script_jack.js
@@ -2,9 +2,7 @@
 window.addEventListener("scroll", function(event) {
     var toBeFixed = document.getElementById("infoToBeFixed");
     var info = document.getElementById("info");
-    var style = toBeFixed.currentStyle || window.getComputedStyle(toBeFixed);
     if (info.getBoundingClientRect().top <= 0 && info.getBoundingClientRect().bottom - window.screen.height*7.6/12 >= 0) {
       toBeFixed.setAttribute("style", "margin-top:" + Math.abs(parseInt(info.getBoundingClientRect().top)) + "px;");
-      console.log( parseInt(style.marginTop + Math.abs(parseInt(info.getBoundingClientRect().top))));
     }
   }, false)
