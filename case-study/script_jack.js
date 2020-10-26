cd4b4728b70e17ce0ecfa3325f979b95ca95b69f
unknown <62070088@kmitl.ac.th>
2020-10-26 18:07:31 +0700

    Added Scroll Capability for really tall images

--- case-study/script_jack.js
+++ case-study/script_jack.js
@@ -1,3 +1,10 @@
-/*window.reonload = function(){
-    window.scrollTo(0, 0);
-}*/
+//For scroll with long image
+window.addEventListener("scroll", function(event) {
+    var toBeFixed = document.getElementById("infoToBeFixed");
+    var info = document.getElementById("info");
+    var style = toBeFixed.currentStyle || window.getComputedStyle(toBeFixed);
+    if (info.getBoundingClientRect().top <= 0 && info.getBoundingClientRect().bottom - window.screen.height*7.6/12 >= 0) {
+      toBeFixed.setAttribute("style", "margin-top:" + Math.abs(parseInt(info.getBoundingClientRect().top)) + "px;");
+      console.log( parseInt(style.marginTop + Math.abs(parseInt(info.getBoundingClientRect().top))));
+    }
+  }, false)
