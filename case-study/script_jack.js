4fb391f512da685e86327b9070381f2a453f67b1
unknown <62070088@kmitl.ac.th>
2020-10-27 21:50:31 +0700

    Fixed responsive scroll capabilities

--- case-study/script_jack.js
+++ case-study/script_jack.js
@@ -1,9 +1,17 @@
 //For scroll with long image, please reload page to test responsive design
 
+var scrollPos = 0;
+var up = false;
+window.addEventListener('scroll', function(){
+  if ((document.body.getBoundingClientRect()).top > scrollPos) up = true;
+	else up = false;
+	scrollPos = (document.body.getBoundingClientRect()).top;
+});
+
 window.addEventListener("scroll", function(event) {
     var toBeFixed = document.getElementById("infoToBeFixed");
     var info = document.getElementById("info");
-    if (info.getBoundingClientRect().top <= 0 && document.getElementById("longImg").getBoundingClientRect().bottom > toBeFixed.getBoundingClientRect().top && window.screen.width >= 1000) {
+    if (info.getBoundingClientRect().top <= 0 && (document.getElementById("longImg").getBoundingClientRect().bottom - toBeFixed.getBoundingClientRect().top > 400 || up) && window.screen.width >= 1000) {
       toBeFixed.setAttribute("style", "margin-top:" + Math.abs(parseInt(info.getBoundingClientRect().top)) + "px;");
     }
   }, false)
