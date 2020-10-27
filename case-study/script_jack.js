91d12ec2c729e2c0ec24fa4140c7c239db2a80eb
unknown <62070088@kmitl.ac.th>
2020-10-27 21:33:38 +0700

    Fixed responsive capabilities

--- case-study/script_jack.js
+++ case-study/script_jack.js
@@ -1,8 +1,9 @@
-//For scroll with long image
+//For scroll with long image, please reload page to test responsive design
+
 window.addEventListener("scroll", function(event) {
     var toBeFixed = document.getElementById("infoToBeFixed");
     var info = document.getElementById("info");
-    if (info.getBoundingClientRect().top <= 0 && info.getBoundingClientRect().bottom - window.screen.height*7.6/12 >= 0) {
+    if (info.getBoundingClientRect().top <= 0 && document.getElementById("longImg").getBoundingClientRect().bottom > toBeFixed.getBoundingClientRect().top && window.screen.width >= 1000) {
       toBeFixed.setAttribute("style", "margin-top:" + Math.abs(parseInt(info.getBoundingClientRect().top)) + "px;");
     }
   }, false)
