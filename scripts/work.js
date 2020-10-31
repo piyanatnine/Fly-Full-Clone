78c76b3dbe0279a832f2a3cbae48ad665abe2871
unknown <62070088@kmitl.ac.th>
2020-10-31 20:20:27 +0700

    Added carousel capabilities

--- scripts/work.js
+++ scripts/work.js
@@ -1,4 +1,11 @@
-/*$('.carousel').carousel({
-    interval: 6000,
-    pause: "false"
-  });*/
+function swipeWork(direction){
+  var carousel = document.getElementById("carouselControl")
+  var nowAt = parseInt(carousel.getAttribute("nowAt"));
+  if(direction == 0){
+    carousel.setAttribute("nowAt", nowAt + (nowAt < 9? 1:0));
+  }
+  else{
+    carousel.setAttribute("nowAt", nowAt - (nowAt > 0? 1:0));
+  }
+  carousel.setAttribute("style", "transform: translateX(" + (-70 * parseInt(carousel.getAttribute("nowAt"))) + "vw);");
+}
