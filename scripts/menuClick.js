27377b3f0e1ba2fcb8b0235d31278ca69195f6ae
unknown <62070088@kmitl.ac.th>
2020-11-08 10:09:17 +0700

    Modified menu

--- scripts/menuClick.js
+++ scripts/menuClick.js
@@ -7,6 +7,7 @@ function menuClick(){
         document.getElementsByClassName("menuClick")[0].setAttribute("style", "margin-right: 0%;visibility: visible;");
         document.getElementsByClassName("shadows")[0].setAttribute("style", "visibility: visible;opacity:0.8;");
         document.getElementsByClassName("textClick")[0].innerHTML = text[1];
+        document.querySelector(".mid").setAttribute("tag", "Visible");
         a = false;
     }
     else {
@@ -14,6 +15,7 @@ function menuClick(){
         document.getElementsByClassName("menuClick")[0].setAttribute("style", "margin-right: -50%;visibility: hidden;");
         document.getElementsByClassName("shadows")[0].setAttribute("style", "visibility: hidden;opacity:0;");
         document.getElementsByClassName("textClick")[0].innerHTML = text[0];
+        document.querySelector(".mid").setAttribute("tag", "");
         a = true;
     }
 };
