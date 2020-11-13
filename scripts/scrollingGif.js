c85c909ae6b83e906b953d56ead73faba9c62f97
unknown <62070088@kmitl.ac.th>
2020-11-13 16:30:53 +0700

    Added scrolling gif in index

---
+++ scripts/scrollingGif.js
@@ -0,0 +1,17 @@
+var scrollingGif = document.getElementsByClassName("scrollingGif");
+window.addEventListener("scroll", function(event) {
+    for(var img of scrollingGif){
+        var style = img.currentStyle || window.getComputedStyle(img);
+        if(img.getAttribute("tag") == null){
+            img.setAttribute("tag", style.top);
+        }
+        var originTop = parseInt(img.getAttribute("tag"));
+        if(up){
+            img.setAttribute("style", "top:" + (parseInt(style.top) >= originTop + 60? originTop + 60: parseInt(style.top) + 1) + "px");
+        }
+        else{
+            img.setAttribute("style", "top:" + (parseInt(style.top) <= originTop - 60? originTop - 60: parseInt(style.top) - 1) + "px");
+        }
+        console.log(style.top);
+    }
+  })
